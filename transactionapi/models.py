aeabdfd2a3e002be241302539c7d71de5c707c25
Karl <karl.turrell@buto.tv>
2016-04-17 12:22:18 +0000

    Relate transactions to users

--- transactionapi/models.py
+++ transactionapi/models.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Transaction(models.Model):
+    user = models.ForeignKey(User)
     date = models.DateTimeField('date occurred')
     description = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=12, decimal_places=2)
